7ffd27cdb530b7476d50782f9827969bc43d331b
[dyad] <gurigaca12@gmail.com>
2025-10-23 14:18:22 +0300

    [dyad] Resolved case-sensitive import error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Index from "./pages/Index";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Admin from "./pages/Admin";
+import Index from "./pages/index"; // Changed from Index
+import Login from "./pages/login"; // Changed from Login
+import Register from "./pages/register"; // Changed from Register
+import Admin from "./pages/admin"; // Changed from Admin
 import { SessionContextProvider } from "./components/SessionContextProvider";
 import { Toaster } from "@/components/ui/sonner";
 
