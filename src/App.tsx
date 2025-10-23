1a7e976d36c7a57504fa0a734225077c35b27877
[dyad] <gurigaca12@gmail.com>
2025-10-23 14:46:26 +0300

    Reverted all changes back to version aa6e9f669c7a0f1a2bac0798be0c4ec0ab287ef8

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Index from "./pages/Index"; // Corrected to capital 'I'
-import Login from "./pages/Login"; // Corrected to capital 'L'
-import Register from "./pages/Register"; // Corrected to capital 'R'
-import Admin from "./pages/Admin"; // Corrected to capital 'A'
-import { SessionContextProvider } from "./components/SessionContextProvider";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login"; // Import the new Login page
+import AdminPage from "./pages/Admin"; // Import the new Admin page
+import { SessionContextProvider } from "./components/SessionContextProvider"; // Import the new context provider
 
-function App() {
-  return (
-    <SessionContextProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Router>
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
       <Toaster />
-    </SessionContextProvider>
-  );
-}
+      <Sonner />
+      <BrowserRouter>
+        <SessionContextProvider> {/* Wrap routes with SessionContextProvider */}
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} /> {/* Add the Login route */}
+            <Route path="/admin" element={<AdminPage />} /> {/* Add the Admin route */}
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </SessionContextProvider>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
