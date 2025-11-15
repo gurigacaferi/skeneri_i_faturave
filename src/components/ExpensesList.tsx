a4e8c58780e4e898c22d6716879a7ccb93aa76b3
[dyad] <gurigaca12@gmail.com>
2025-11-15 16:38:26 +0200

    [dyad] Adding 'use client' to ReceiptUpload and ExpensesList to fix context error. - wrote 2 file(s)

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useEffect, useState, useCallback } from 'react';
 import { useSession } from '@/components/SessionContextProvider';
 import {
--- src/components/ReceiptUpload.tsx
+++ src/components/ReceiptUpload.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useCallback, useMemo } from 'react';
 import { useSession } from '@/components/SessionContextProvider';
 import { Button } from '@/components/ui/button';
